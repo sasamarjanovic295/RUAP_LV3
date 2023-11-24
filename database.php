7589a38aeea02f8ec36be67625f7abc13f94c728
Sasa Marjanovic <sasa.marjanovic295@gmail.com>
2023-11-24 15:34:41 +0100

    Remove creating database code

--- database.php
+++ database.php
@@ -14,10 +14,6 @@ if (mysqli_connect_errno())
 echo "Failed to connect to MySQL: " . mysqli_connect_error(); }
 else {
 echo 'Connected successfully'; }
-$sql = 'CREATE Database ruapdb'; if ($conn->query($sql) === TRUE) {
-echo "Database created successfully"; } else {
-echo "Error creating database: " . $conn->error; }
-mysqli_select_db($conn,$db);
 $sql_create = "CREATE TABLE registration_tbl(id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(30), email VARCHAR(30), date DATE);";
 if ($conn->query($sql_create) === TRUE) {
 echo "Table registration_tbl created successfully";
